8e4183345370028b099b4dd46fb6c6462d696027
Richard5P <richard.deutsch@gmail.com>
2023-09-19 14:40:51 +0000

    feat: Add additional question to questions.js

--- questions.js
+++ questions.js
@@ -1,27 +1,37 @@
-// Question/Answer Object Data
-
 const quizQuestions = [{
-        question: "\'There are decades where nothing happens; and there are weeks where decades happen.\' Is a quote from?",
-        a: "Karl Marx",
-        b: "Leon Trosky",
-        c: "Rosa Luxemburg",
-        d: "Vladimir Lenin",
-        answer: "d",
-        linkto: {
-            docName: "War and Revolution",
-            url: "https://www.marxists.org/archive/lenin/works/1917/may/14.htm",
-        }
-    },
-    {
-        question: "\'A specter is haunting Europe—the specter of Communism.\' was first published in?",
-        a: "Das Kapital",
-        b: "Neue Rheinische Zeitung",
-        c: "The Communist Manifesto",
-        d: "Founding Documents of the Communist League",
-        answer: "c",
-        linkto: {
-            docName: "The Communist Manifesto",
-            url: "https://www.marxists.org/archive/marx/works/1848/communist-manifesto/",
-        },
+    question: "\'There are decades where nothing happens; and there are weeks where decades happen.\' Is a quote from?",
+    a: "Karl Marx",
+    b: "Leon Trosky",
+    c: "Rosa Luxemburg",
+    d: "Vladimir Lenin",
+    answer: "d",
+    linkto: {
+        docName: "War and Revolution",
+        url: "https://www.marxists.org/archive/lenin/works/1917/may/14.htm",
     }
+},
+{
+    question: "\'A specter is haunting Europe—the specter of Communism.\' was first published in?",
+    a: "Das Kapital",
+    b: "Neue Rheinische Zeitung",
+    c: "The Communist Manifesto",
+    d: "Founding Documents of the Communist League",
+    answer: "c",
+    linkto: {
+        docName: "The Communist Manifesto",
+        url: "https://www.marxists.org/archive/marx/works/1848/communist-manifesto/",
+    },
+},
+{
+    question: "Marx turned the work of which philosopher \'on it\'s head?\'?",
+    a: "Joseph Proudhon",
+    b: "Jacques Rousseau",
+    c: "Adam Smith",
+    d: "Friedrich Hegel",
+    answer: "d",
+    linkto: {
+        docName: "The Communist Manifesto",
+        url: "https://www.marxists.org/archive/marx/works/1848/communist-manifesto/",
+    },
+},
 ]
